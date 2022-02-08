596712bd30dd8d1fd18a8d468478048d78a31c97
Dan Baechtold <latebloomertech@gmail.com>
2022-02-08 16:56:22 -0500

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,50 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toyItems, setToyItems] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+    .then((res) => res.json())
+    .then((data) => setToyItems(data))
+  }, []);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function handleAddItem(newItem) {
+    setToyItems([...toyItems, newItem]);
+  }
+
+  function handleDeleteToy(toyToDelete) {
+    const updatedItems = toyItems.filter((item) => item.id !== toyToDelete.id);
+    setToyItems(updatedItems);
+  }
+
+  function handleUpdateToy(updatedToy) {
+      const updatedItems = toyItems.map((item) => {
+        if (item.id === updatedToy.id) {
+          return updatedToy
+        } else {
+          return item;
+        }
+      });
+      setToyItems(updatedItems);
+    }
+    
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onAddItem={handleAddItem}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer
+          toyItems={toyItems}
+          onDeleteToy={handleDeleteToy}
+          onUpdateToy={handleUpdateToy}
+      />
     </>
   );
 }
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,22 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ToyForm() {
+
+function ToyForm({onAddItem}) {
+const [name, setName] = useState("")
+const [image, setImage] = useState("")
+
+function handleFormSubmit(e) {
+  e.preventDefault()
+  const itemData = {
+    name: name,
+    image: image,
+    likes: 0
+  };
+
+  fetch("http://localhost:3001/toys", {
+    method: "POST",
+    headers: {
+      "Content-Type" : "application/json",
+    },
+    body: JSON.stringify(itemData)
+  })
+  .then((res) => res.json())
+  .then((newItem) => onAddItem(newItem));
+
+}
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleFormSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
-          name="name"
+          name={name}
           placeholder="Enter a toy's name..."
           className="input-text"
+          onChange={(e) => setName(e.target.value)}
         />
         <br />
         <input
           type="text"
-          name="image"
+          name={image}
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          onChange={(e) => setImage(e.target.value)}
         />
         <br />
         <input
